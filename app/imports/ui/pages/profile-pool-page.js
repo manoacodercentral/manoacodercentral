7319a1a4a3151d7576a81a4fabedbacd2d3283c1
Cassandra Largosa <cjlargos@hawaii.edu>
2017-04-12 14:41:15 -1000

    Added filter options.

--- app/imports/ui/pages/profile-pool-page.js
+++ app/imports/ui/pages/profile-pool-page.js
@@ -1,3 +1,21 @@
 import { Template } from 'meteor/templating';
 import { _ } from 'meteor/underscore';
 import { languageList, osList, timeList } from './edit-profile-page';
+
+Template.Profile_Pool_Page.helpers({
+  planguages() {
+    return _.map(languageList, function makeInstrumentObject(planguage) {
+      return { label: planguage };
+    });
+  },
+  osystems() {
+    return _.map(osList, function makeInstrumentObject(os) {
+      return { label: os };
+    });
+  },
+  times() {
+    return _.map(timeList, function makeInstrumentObject(time) {
+      return { label: time };
+    });
+  },
+});
