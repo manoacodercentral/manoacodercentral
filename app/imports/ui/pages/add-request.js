6097f415652e03302ae35b71a824153f087b7295
Cassandra Largosa <cjlargos@hawaii.edu>
2017-04-16 17:52:46 -1000

    Implemented OS Collection into Add Request Page.
    Replaced the OS old dropdown to use form control and pulled the list
    items from OS Collection.

--- app/imports/ui/pages/add-request.js
+++ app/imports/ui/pages/add-request.js
@@ -2,6 +2,21 @@
  * Created by jake on 4/11/17.
  */
 
+import { Template } from 'meteor/templating';
+import { _ } from 'meteor/underscore';
+import { OS } from '../../api/os/os.js';
+
 $('.ui.radio.checkbox')
     .checkbox()
 ;
+
+Template.Add_Request.helpers({
+  osystems() {
+    return _.map(OS.find().fetch(),
+        function makeOSObject(os) {
+          return {
+            label: os.name,
+          };
+        });
+  },
+});
